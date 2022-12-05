eb1a78f41669efe44fdbb6943e0ec09ac2d90c0c
Satyabrat Jha <satyabrat.jha@vidyamantra.com>
2022-12-05 16:50:04 +0530

    initial commit

---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="task">
+        <h3>{{ props.task.title }}</h3>
+        <div class="icons">
+            <i class="material-icons" @click="taskStore.deleteTask(task.id)">delete</i>
+            <i class="material-icons" @click="taskStore.toggleFav(task.id)" :class="{active: task.isFav}">favorite</i>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useTaskStore } from "../stores/TaskStore"
+
+    const props = defineProps(['task'])
+
+    const taskStore = useTaskStore()
+
+</script>
+
+<style>
+
+</style>
